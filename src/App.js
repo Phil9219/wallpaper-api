34575fc1afead5107383d162a75fa6ca0f7e1f55
Phil9219 <philipp.fell@yahoo.de>
2020-10-27 15:52:30 +0100

    Add Image and css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import "./App.css";
+import "./global.css";
+import ImagePreview from "./components/ImagePreview.";
 
 function App() {
-  return <></>;
+  return (
+    <main>
+      <button>Get Random Image</button>
+
+      <ImagePreview
+        src={
+          "https://images.unsplash.com/photo-1593362977357-4ed93b09bd1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=60"
+        }
+        alt={"Autum"}
+        author={"Philipp"}
+      />
+    </main>
+  );
 }
 
 export default App;
